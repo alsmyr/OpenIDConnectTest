1d82ba17215e14640b5de0805aed6c63d2a9e5d6
alsmyr <erik.alsmyr@accelrys.com>
2014-04-11 10:46:16 +0200

    Initial commit

---
+++ OpenIDConnectTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenIDConnectTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Protected()
+        {
+            ViewBag.Message = "This is a Protected Page!";
+
+            return View();
+        }
+
+        
+    }
+}
